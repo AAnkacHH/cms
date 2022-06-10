2c7374f4141182223dd49d5eb11ca612cde62ddc
Andrii Plyskach <ankach.ua@gmail.com>
2022-06-10 13:40:34 +0200

    Initial commit.

---
+++ src/main/java/cz/ankach/cms/ApplicationLauncher.java
@@ -0,0 +1,13 @@
+package cz.ankach.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLauncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationLauncher.class, args);
+	}
+
+}
