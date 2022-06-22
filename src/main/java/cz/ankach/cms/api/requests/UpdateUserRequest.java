6014791d162d7461c04244ae47577a27f6befcf6
Andrii Plyskach <ankach.ua@gmail.com>
2022-06-22 20:55:37 +0200

    [api] Adding update user request.

---
+++ src/main/java/cz/ankach/cms/api/requests/UpdateUserRequest.java
@@ -0,0 +1,10 @@
+package cz.ankach.cms.api.requests;
+
+import java.util.List;
+
+public class UpdateUserRequest {
+    public String firstname;
+    public String lastname;
+
+    public List<String> roles;
+}
