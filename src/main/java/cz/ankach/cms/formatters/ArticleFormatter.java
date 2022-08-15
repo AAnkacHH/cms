f3077bb574ddca37f601ccf293a6d2fc03a26aa6
Andrii Plyskach <ankach.ua@gmail.com>
2022-08-15 23:43:33 +0200

    [formatter] Adding comment and update article formatters.

--- src/main/java/cz/ankach/cms/formatters/ArticleFormatter.java
+++ src/main/java/cz/ankach/cms/formatters/ArticleFormatter.java
@@ -1,6 +1,7 @@
 package cz.ankach.cms.formatters;
 
 import cz.ankach.cms.api.responses.ArticleResponse;
+import cz.ankach.cms.api.responses.CommentResponse;
 import cz.ankach.cms.entity.Article;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +10,11 @@ import java.util.List;
 @Service
 public class ArticleFormatter {
     private final UserFormatter userFormatter;
+    private final CommentFormatter commentFormatter;
 
-    public ArticleFormatter(UserFormatter userFormatter) {
+    public ArticleFormatter(UserFormatter userFormatter, CommentFormatter commentFormatter) {
         this.userFormatter = userFormatter;
+        this.commentFormatter = commentFormatter;
     }
 
     /**
@@ -20,26 +23,19 @@ public class ArticleFormatter {
      * @return ArticleResponse
      * */
     public ArticleResponse format(Article article) {
-
+        List<CommentResponse> comments = article.getComments().stream().map(this.commentFormatter::format).toList();
         var userResponse = this.userFormatter.format(article.getAuthor());
-        if (article.getParent() != null) {
-            return new ArticleResponse(
-                    article.getTitle(),
-                    article.getContent(),
-                    userResponse,
-                    article.getOrder(),
-                    article.getCreatedAt(),
-                    article.getUpdatedAt(),
-                    this.format(article.getParent())
-            );
-        }
+        var parent = article.getParent();
+
         return new ArticleResponse(
                 article.getTitle(),
                 article.getContent(),
                 userResponse,
                 article.getOrder(),
                 article.getCreatedAt(),
-                article.getUpdatedAt()
+                article.getUpdatedAt(),
+                parent != null ? this.format(parent) : null,
+                comments
         );
     }
 
--- src/main/java/cz/ankach/cms/formatters/CommentFormatter.java
+++ src/main/java/cz/ankach/cms/formatters/CommentFormatter.java
@@ -1,4 +1,19 @@
 package cz.ankach.cms.formatters;
 
+import cz.ankach.cms.api.responses.CommentResponse;
+import cz.ankach.cms.entity.Comment;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentFormatter {
+    public CommentResponse format(Comment comment) {
+        return new CommentResponse(
+                comment.getId(),
+                comment.getContent(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt(),
+                comment.getAuthor().getUsername(),
+                comment.getParent() != null ? this.format(comment.getParent()) : null
+        );
+    }
 }
