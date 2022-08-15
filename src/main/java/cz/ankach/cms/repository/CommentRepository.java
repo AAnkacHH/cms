56ed68de93eb2f92830848ac0aa72a247b18a7e2
Andrii Plyskach <ankach.ua@gmail.com>
2022-08-15 23:41:22 +0200

    [service] Update existing services.

--- src/main/java/cz/ankach/cms/repository/CommentRepository.java
+++ src/main/java/cz/ankach/cms/repository/CommentRepository.java
@@ -1,2 +1,10 @@
-package cz.ankach.cms.repository;public interface CommentRepository {
+package cz.ankach.cms.repository;
+
+import cz.ankach.cms.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
 }
